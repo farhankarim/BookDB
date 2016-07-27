7f7ca24c857d0c8fffa25a602049008dff23f933
UNKNOWN <you@example.com>
2016-07-27 18:48:29 +0500

    ADDED ALL THE BASIC STUFF (MODEL,ADAPTER,HTTPCLIENT,CUSTOM ACTIONBAR)

---
+++ app/src/main/java/android/unknown/com/booksearchscratch/BookListActivity.java
@@ -0,0 +1,111 @@
+package android.unknown.com.booksearchscratch;
+
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.unknown.com.booksearchscratch.Adapter.BookAdapter;
+import android.unknown.com.booksearchscratch.HTTPclient.BookClient;
+import android.unknown.com.booksearchscratch.Model.Book;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class BookListActivity extends AppCompatActivity {
+    private BookClient client;
+    private ListView lvBooks;
+    private BookAdapter bookAdapter;
+    private ProgressBar progress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+        progress = (ProgressBar) findViewById(R.id.progress);
+        lvBooks = (ListView) findViewById(R.id.lvBooks);
+        ArrayList<Book> aBooks = new ArrayList<Book>();
+        bookAdapter = new BookAdapter(this, aBooks);
+        lvBooks.setAdapter(bookAdapter);
+
+        //FOR OSCAR WILDE LOVERS ONLY
+        //fetchBooks("Oscar Wilde");
+
+    }
+
+    // Executes an API call to the OpenLibrary search endpoint, parses the results
+    // Converts them into an array of book objects and adds them to the adapter
+    private void fetchBooks(String query) {
+        progress.setVisibility(ProgressBar.VISIBLE);
+        client = new BookClient();
+        client.getBooks(query, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    progress.setVisibility(ProgressBar.GONE);
+                    JSONArray docs = null;
+                    if (response != null) {
+                        // Get the docs json array
+                        docs = response.getJSONArray("docs");
+                        // Parse json array into array of model objects
+                        final ArrayList<Book> books = Book.fromJson(docs);
+                        // Remove all books from the adapter
+                        bookAdapter.clear();
+                        // Load model objects into the adapter
+                        for (Book book : books) {
+                            bookAdapter.add(book); // add book through the adapter
+                        }
+                        bookAdapter.notifyDataSetChanged();
+                    }
+                } catch (JSONException e) {
+                    // Invalid JSON format, show appropriate error.
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                progress.setVisibility(ProgressBar.GONE);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_list, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Fetch the data remotely
+                fetchBooks(query);
+                // Reset SearchView
+                searchView.clearFocus();
+                searchView.setQuery("", false);
+                searchView.setIconified(true);
+                searchItem.collapseActionView();
+                // Set activity title to search query
+                BookListActivity.this.setTitle(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return true;
+    }
+}
---
+++ app/src/main/java/android/unknown/com/booksearchscratch/HTTPclient/BookClient.java
@@ -0,0 +1,33 @@
+package android.unknown.com.booksearchscratch.HTTPclient;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/*Java class that will act as OpenLibrary API client for
+** sending out network requests to specific endpoints. */
+
+public class BookClient {
+    private static final String API_BASE_URL = "http://openlibrary.org/";
+    private AsyncHttpClient client;
+
+    public BookClient() {
+        this.client = new AsyncHttpClient();
+    }
+
+    private String getApiUrl(String relativeUrl) {
+        return API_BASE_URL + relativeUrl;
+    }
+
+    // Method for accessing the search API
+    public void getBooks(final String query, JsonHttpResponseHandler handler) {
+        try {
+            String url = getApiUrl("search.json?q=");
+            client.get(url + URLEncoder.encode(query, "utf-8"), handler);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
